3c1b5bae6002aab90b0e0c07110f3d87e72c9333
LordAmoSupremo <125117427+LordAmoSupremo@users.noreply.github.com>
2024-05-13 22:07:32 -0600

    Add files via upload

---
+++ cliinica.sql
@@ -0,0 +1,142 @@
+CREATE DATABASE  IF NOT EXISTS `clinica` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `clinica`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: clinica
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `agendar_citas`
+--
+
+DROP TABLE IF EXISTS `agendar_citas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `agendar_citas` (
+  `ID_cita` int NOT NULL AUTO_INCREMENT,
+  `ID_paciente` int NOT NULL,
+  `Fecha_cita` date NOT NULL,
+  `Hora_cita` time NOT NULL,
+  `ID_doctor` int NOT NULL,
+  `Motivo` varchar(150) NOT NULL,
+  PRIMARY KEY (`ID_cita`),
+  KEY `agendar_citas_ibfk_1` (`ID_paciente`),
+  KEY `agendar_citas_ibfk_2` (`ID_doctor`),
+  CONSTRAINT `agendar_citas_ibfk_1` FOREIGN KEY (`ID_paciente`) REFERENCES `paciente` (`ID_paciente`),
+  CONSTRAINT `agendar_citas_ibfk_2` FOREIGN KEY (`ID_doctor`) REFERENCES `doctor` (`ID_doctor`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agendar_citas`
+--
+
+LOCK TABLES `agendar_citas` WRITE;
+/*!40000 ALTER TABLE `agendar_citas` DISABLE KEYS */;
+INSERT INTO `agendar_citas` VALUES (1,1,'2024-05-09','10:00:00',1,'Consulta de seguimiento'),(2,2,'2024-05-08','09:00:00',2,'Control de peso y generación de dieta'),(3,3,'2024-05-20','10:30:00',3,'Remover caries y endodoncia');
+/*!40000 ALTER TABLE `agendar_citas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctor` (
+  `ID_doctor` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(25) NOT NULL,
+  `Apellido` varchar(25) NOT NULL DEFAULT ' NULL',
+  `Especialidad` varchar(25) NOT NULL,
+  PRIMARY KEY (`ID_doctor`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctor`
+--
+
+LOCK TABLES `doctor` WRITE;
+/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
+INSERT INTO `doctor` VALUES (1,'Julian','Mendoza','Alergologia'),(2,'David','Pérez','Nutricion'),(3,'Samuel','Lopez','Dentista');
+/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paciente`
+--
+
+DROP TABLE IF EXISTS `paciente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paciente` (
+  `ID_paciente` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(25) NOT NULL,
+  `Apellido` varchar(25) NOT NULL,
+  `Edad` int NOT NULL,
+  `Altura` int NOT NULL,
+  `Peso` int NOT NULL,
+  PRIMARY KEY (`ID_paciente`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paciente`
+--
+
+LOCK TABLES `paciente` WRITE;
+/*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
+INSERT INTO `paciente` VALUES (1,'Juan','Pérez',30,170,70),(2,'Itzell','Treviño',20,165,80),(3,'Karen','Luna',15,150,53);
+/*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `ID_usuario` int NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `contrasena` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID_usuario`),
+  UNIQUE KEY `nombre_usuario` (`nombre_usuario`),
+  UNIQUE KEY `contrasena` (`contrasena`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'ADMIN01','Raptor556#');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-13 22:06:44
